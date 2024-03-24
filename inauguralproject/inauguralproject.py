7cbc634f96d40f2ab8b35e172bd8b8d8430e4877
drp193 <drp193@alumni.ku.dk>
2024-03-24 21:15:42 +0900

    q3

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,7 +1,3 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.optimize import fsolve
-from scipy.optimize import minimize
 from types import SimpleNamespace
 
 class MarketModel():
