7b641e86cca2b3ece4bf4da317e5029f7205a58f
drp193 <drp193@alumni.ku.dk>
2024-05-02 11:17:14 +0200

    model

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -64,64 +64,14 @@ best_response_diff_R = sm.diff(objective_diff, q_R)
 jacobian_q_i = sm.lambdify((q, q_R), best_response_diff_i)
 jacobian_q_R = sm.lambdify((q, q_R), best_response_diff_R)
 
-def h(q_values, q_R_values, c_values, b_value, m_value):
-    return np.array([best_response(q_values[i], q_R_values[i], c_values[i], b_value, m_value) for i in range(len(q_values))])
+equilibrium_condition = best_response - (q + q_R)
 
-def hp(q_values, q_R_values, b_value):
-    N = len(q_values)
-    jacobian_matrix = np.zeros((N, N))
-    for i in range(N):
-        for j in range(N):
-            if j == i:
-                jacobian_matrix[i, j] = jacobian_q_i(q_values[i], q_R_values[i])
-            else:
-                jacobian_matrix[i, j] = jacobian_q_R(q_values[i], q_R_values[i])
-    return jacobian_matrix
+# Solving for equilibrium quantities
+equilibrium_solution = sm.solve(equilibrium_condition, q)
 
-def solve_model(N=50, b=1, seed=2000, draw_from_distribution=True, constant_value=9999, display=True):
-    N_init = N
+# Create lambdified functions
+equilibrium_func = sm.lambdify((q_R, c, b, m), equilibrium_solution)
 
-    if draw_from_distribution:
-        np.random.seed(seed)
-        c_values = 0.01 * np.random.lognormal(mean=0, sigma=1, size=N)
-    else:
-        c_values = np.full((N,), constant_value)
-
-    c_values_init = c_values.copy()
-
-    x = np.zeros(N)
-    x_nonneg = np.zeros(N, dtype=bool)
-
-    while not all(x_nonneg):
-        result = root(lambda x: h(x, np.sum(x) - x, c_values, b, 0.5), x, jac=lambda x: hp(x, np.sum(x)-x, b))
-        x = result.x
-        x_nonneg = (x >= 0).astype(bool)
-        
-        c_values = c_values[x_nonneg]
-        x = x[x_nonneg]
-        N = np.sum(x_nonneg)
-
-    profit = objective_lambd(result.x, np.sum(result.x) - result.x, c_values, b, 0.5)
-
-    if display:
-        print(result)
-        print('\nx =', result.x[0:5], '\nh(x) =', h(result.x, c_values, b, 0.5)[0:5], '\nsum(x) =', np.sum(result.x), '\ncost =', c_values[0:5],'\nProfit =', profit[0:5],'\nb =', b,'\nN_firms =', N)
-
-    for i in range(N_init):
-        if i in result.success:
-            continue
-        else:
-            x = np.insert(x, i, 0)
-            profit = np.insert(profit, i, 0)
-
-    sol = SimpleNamespace()
-    sol.c_values_init = c_values_init
-    sol.c_values = c_values
-    sol.x = x
-    sol.N = N
-    sol.N_init = N_init
-    sol.profit = profit
-
-    return sol
-
-
+def market_equilibrium_model(q_R_values, c_value, b_value, m_value):
+    equilibrium_q_i = equilibrium_func(q_R_values, c_value, b_value, m_value)
+    return equilibrium_q_i
