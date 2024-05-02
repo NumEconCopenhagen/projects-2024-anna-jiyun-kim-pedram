45007c8c78f96cf997d67140aef575a486a43bd5
drp193 <drp193@alumni.ku.dk>
2024-05-02 11:00:40 +0200

    mp

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -3,6 +3,8 @@ import numpy as np
 import sympy as sm
 import matplotlib.pyplot as plt
 from types import SimpleNamespace
+from scipy.optimize import root
+
 
 
 def solve_ss(alpha, c):
@@ -34,110 +36,92 @@ c = sm.symbols("c") #Cost
 m = sm.symbols("m") #Parameter for the slope of the price function
 b = sm.symbols("b") # Intercept of the price function
 
-#Define price
-price_function = sm.Eq(p, m*(q+q_R) + b)
+# Define price function
+price_function = sm.Eq(p, m * (q + q_R) + b)
 
-#Define profit
-profit_function = p*q - c*q
+# Define profit function
+profit_function = p * q - c * q
 
-#Solve the price
+# Solve for price function
 p_eq = sm.solve(price_function, p)
 
-#Sub price and create objective
-objec = profit_function.subs(p,p_eq[0])
+# Substitute price and create objective function
+objective = profit_function.subs(p, p_eq[0])
 
-#Lambdify functions
-objec_l = sm.lambdify(args=(q, q_R, c, b, m), expr = objec)
+# Lambdify functions
+objective_lambd = sm.lambdify((q, q_R, c, b, m), objective)
 
-#FOC profit wrt. quantity
-objec_diff = sm.diff(objec, q)
+# First-order derivative of profit function with respect to quantity
+objective_diff = sm.diff(objective, q)
 
-#Best Response (BR)
-bestR = sm.lambdify(args=(q, q_R, c, b, m), expr = objec_diff)
+# Best Response function
+best_response = sm.lambdify((q, q_R, c, b, m), objective_diff)
 
-#second derivatives generated for generating jacobian matrix for firm i and rest
-bestR_diff_i = sm.diff(objec_diff, q)
-bestR_diff_R = sm.diff(objec_diff, q_R)
+# Second derivatives for generating the Jacobian matrix for firm i and rest
+best_response_diff_i = sm.diff(objective_diff, q)
+best_response_diff_R = sm.diff(objective_diff, q_R)
 
-jacob_q_i = sm.lambdify(args=(q, q_R), expr = bestR_diff_i)
-jacob_q_R = sm.lambdify(args=(q, q_R), expr = bestR_diff_R)
+jacobian_q_i = sm.lambdify((q, q_R), best_response_diff_i)
+jacobian_q_R = sm.lambdify((q, q_R), best_response_diff_R)
 
+def h(q_values, q_R_values, c_values, b_value, m_value):
+    return np.array([best_response(q_values[i], q_R_values[i], c_values[i], b_value, m_value) for i in range(len(q_values))])
 
-# Calculating the best response of each firm for optimization
-def h(q, c_vec, N, b, m):
-    y = np.zeros(N)
-    for i in range(N):
-
-        y[i] = bestR(q[i], sum(q) - q[i], c_vec[i], b, m)
-    return y
-
-
-# Optimizing the jacobian
-def hp(q, N):
-    y = np.zeros((N, N))
+def hp(q_values, q_R_values, b_value):
+    N = len(q_values)
+    jacobian_matrix = np.zeros((N, N))
     for i in range(N):
         for j in range(N):
             if j == i:
-                y[i, j] = jacob_q_i(q[i], sum(q)-q[i])
+                jacobian_matrix[i, j] = jacobian_q_i(q_values[i], q_R_values[i])
             else:
-                y[i, j] = jacob_q_R(q[i], sum(q)-q[i])
-    return y
+                jacobian_matrix[i, j] = jacobian_q_R(q_values[i], q_R_values[i])
+    return jacobian_matrix
 
-# using scipy.optimize.root to solve and find the market equilibrium
-def solve_model(N=50, seed=2000, draw_from_distribution=True, constant_value=9999, display=True):
-
+def solve_model(N=50, b=1, seed=2000, draw_from_distribution=True, constant_value=9999, display=True):
     N_init = N
 
-    # if/else statement to draw from log normal distribution
     if draw_from_distribution:
         np.random.seed(seed)
-        c_vec = np.random.lognormal(mean=0, sigma=1, size=N)
+        c_values = 0.01 * np.random.lognormal(mean=0, sigma=1, size=N)
     else:
-        c_vec = np.full((N,), constant_value)
+        c_values = np.full((N,), constant_value)
 
-    c_vec_init = c_vec.copy()
+    c_values_init = c_values.copy()
 
-    # Setting up the initial values for q
-    index = np.array(range(N))
-    q0 = np.zeros(N)
-    q_nonneg = np.zeros(N, dtype=bool)
+    x = np.zeros(N)
+    x_nonneg = np.zeros(N, dtype=bool)
 
-    while not all(q_nonneg):
-
-        # Solving the optimization problem using scipy.optimize.root() function
-        result = optimize.root(lambda q0: h(q0, c_vec, N, b, m), q0, jac=lambda q0: hp(q0, N))
-        q0 = result.q
-        q_nonneg = (q0 >= 0).astype(bool)
+    while not all(x_nonneg):
+        result = root(lambda x: h(x, np.sum(x) - x, c_values, b, 0.5), x, jac=lambda x: hp(x, np.sum(x)-x, b))
+        x = result.x
+        x_nonneg = (x >= 0).astype(bool)
         
-        c_vec = c_vec[q_nonneg]
-        q0    = q0[q_nonneg]
-        N     = np.sum(q_nonneg)
-        index = index[q_nonneg]
+        c_values = c_values[x_nonneg]
+        x = x[x_nonneg]
+        N = np.sum(x_nonneg)
 
-    profit=objec_l(result.q, np.sum(result.q)-result.q, c_vec)
+    profit = objective_lambd(result.x, np.sum(result.x) - result.x, c_values, b, 0.5)
 
-    # Printing the results
-    if display == True:
+    if display:
         print(result)
-        print('\nq =', result.q[0:5], '\nh(q) =', h(q0,c_vec,N)[0:5], '\nsum(q) =', sum(result.q), '\nmarginal cost=',c_vec[0:5],'\nprofit=', profit[0:5],'\nN_firms =',N)
+        print('\nx =', result.x[0:5], '\nh(x) =', h(result.x, c_values, b, 0.5)[0:5], '\nsum(x) =', np.sum(result.x), '\ncost =', c_values[0:5],'\nProfit =', profit[0:5],'\nb =', b,'\nN_firms =', N)
 
     for i in range(N_init):
-        if i in index:
+        if i in result.success:
             continue
         else:
-            q0=np.insert(q0,i,0)
-            profit=np.insert(profit,i,0)
+            x = np.insert(x, i, 0)
+            profit = np.insert(profit, i, 0)
 
     sol = SimpleNamespace()
-    sol.c_vec_init=c_vec_init
-    sol.c_vec=c_vec
-    sol.q0=q0
-    sol.index=index
-    sol.N=N
-    sol.N_init=N_init
-    sol.profit=profit
+    sol.c_values_init = c_values_init
+    sol.c_values = c_values
+    sol.x = x
+    sol.N = N
+    sol.N_init = N_init
+    sol.profit = profit
 
     return sol
-
 
 
